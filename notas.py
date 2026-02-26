8cf792b09bd56d1df48c689fb5ee859343757ced
Sergio <sergiogaleanob2008@gmail.com>
2026-02-26 10:45:04 -0500

    inicializando proyecyto notas

---
+++ notas.py
@@ -0,0 +1,9 @@
+
+estudiantes = [
+    {"nombre" : "Andres", "notas": [3.0, 4.5, 4.0]},
+    {"nombre" : "Fabio", "notas": [4.0, 5.0, 3.5]},
+    {"nombre" : "maria", "notas": [2.9, 3.0, 3.6]}
+]
+
+print("----datos cargados----")
+print (f"total de estudiantes: {len(estudiantes)}")
